977645568bdb115223ff009e5dd76fc1afd9740d
banqingyuan <15606160125@163.com>
2019-12-06 22:24:36 +0800

    12月6号

--- src/main/resources/static/js/action.js
+++ src/main/resources/static/js/action.js
@@ -1,7 +1,7 @@
 // JavaScript Document
         function do_static() {
             $.ajax({
-                url: 'http://127.0.0.1:8080/login.html',
+                url: '/login.html',
                 type: 'GET',
                 /*data: 'username=admin&password=123',*/
                 async: false,
@@ -19,7 +19,7 @@
 
         function do_test() {
             $.ajax({
-                url: 'http://127.0.0.1:8080/test/session',
+                url: '/test/session',
                 type: 'GET',
                 /*data: 'username=admin&password=123',*/
                 async: false,
@@ -37,7 +37,7 @@
 		function do_searchGoods(){
 			var param = $('#goodsName').val()
 			$.ajax({
-				url:'http://127.0.0.1:8080/searchGoods?goodsName='+param,
+				url:'/searchGoods?goodsName='+param,
 				type:'GET',
 				async:false,
 				cache:false,
@@ -51,10 +51,11 @@
                 }
 			});
 		}
+
         function do_buyerRegister() {
             var param={"name":$('#register_buyer_name').val(),"password":$('#register_buyer_password').val(),"department":$('#register_buyer_department').val(),"address":$('#register_buyer_address').val(),"email":$('#register_buyer_email').val()}
             $.ajax({
-                url: 'http://127.0.0.1:8080/buyerRegister',
+                url: '/buyerRegister',
                 type: 'POST',
                 data:JSON.stringify(param),
                 async: false,
@@ -65,7 +66,7 @@
                     if(returndata==true){
 						alert("注册成功！");
 						$("#user_ul").empty();
-						$("#user_ul").append('<li><a href="#" data-toggle="modal"><span class="fa fa-unlock-alt" aria-hidden="true"></span><span id="user_email_span"></span></a></li>');
+						$("#user_ul").append('<li><a href="#" data-toggle="modal" data-target="#myModal5"><span class="fa fa-unlock-alt" aria-hidden="true"></span><span id="user_email_span"></span></a></li>');
 						$("#user_email_span").append(param.email);
 					}else{
 						alert("注册失败");
@@ -79,7 +80,7 @@
         function do_supplierRegister() {
             var param={"name":$('#register_supplier_name').val(),"password":$('#register_supplier_password').val(),"department":$('#register_supplier_department').val(),"address":$('#register_supplier_address').val(),"email":$('#register_supplier_email').val()}
             $.ajax({
-                url: 'http://127.0.0.1:8080/supplierRegister',
+                url: '/supplierRegister',
                 type: 'POST',
                 data:JSON.stringify(param),
                 async: false,
@@ -90,8 +91,9 @@
                     if(returndata==true){
 						alert("注册成功！");
 						$("#user_ul").empty();
-						$("#user_ul").append('<li><a href="#" data-toggle="modal"><span class="fa fa-unlock-alt" aria-hidden="true"></span><span id="user_email_span"></span></a></li>');
+						$("#user_ul").append('<li><a href="#" data-toggle="modal" data-target="#myModal6"><span class="fa fa-unlock-alt" aria-hidden="true"></span><span id="user_email_span"></span></a></li>');
 						$("#user_email_span").append(param.email);
+
 					}else{
 						alert("注册失败");
 					}
@@ -104,7 +106,7 @@
         function do_loginSupplier() {
             var param={"password":$('#supplier_password').val(),"email":$('#supplier_email').val()}
             $.ajax({
-                url: 'http://127.0.0.1:8080/supplierLogin',
+                url: '/supplierLogin',
                 type: 'POST',
                 data: JSON.stringify(param),
                 async: false,
@@ -115,7 +117,7 @@
 					if(returndata==true){
 						alert("登录成功");
 						$("#user_ul").empty();
-						$("#user_ul").append('<li><a href="#" data-toggle="modal"><span class="fa fa-unlock-alt" aria-hidden="true"></span><span id="user_email_span"></span></a></li>');
+						$("#user_ul").append('<li><a href="#" data-toggle="modal" data-target="#myModal6"><span class="fa fa-unlock-alt" aria-hidden="true"></span><span id="user_email_span"></span></a></li>');
 						$("#user_email_span").append(param.email);
 					}else{
 						alert("登录失败");
@@ -129,7 +131,7 @@
         function do_loginBuyer() {
             var param={"password":$('#buyer_password').val(),"email":$('#buyer_email').val()};
             $.ajax({
-                url: 'http://127.0.0.1:8080/buyerLogin',
+                url: '/buyerLogin',
                 type: 'POST',
                 data: JSON.stringify(param),
                 async: false,
@@ -140,8 +142,10 @@
 					if(returndata==true){
 						alert("登录成功");
 						$("#user_ul").empty();
-						$("#user_ul").append('<li><a href="#" data-toggle="modal"><span class="fa fa-unlock-alt" aria-hidden="true"></span><span id="user_email_span"></span></a></li>');
+						$("#user_ul").append('<li><a href="#" data-toggle="modal" data-target="#myModal5" ><span class="fa fa-unlock-alt" aria-hidden="true"></span><span id="user_email_span"></span></a></li>');
 						$("#user_email_span").append(param.email);
+						do_searchOrder();
+
 					}else{
 						alert("登录失败");
 					}
@@ -153,7 +157,7 @@
         }
         function do_search() {
             $.ajax({
-                url: 'http://127.0.0.1:8080/searchGoods',
+                url: '/searchGoods',
                 type: 'GET',
                 async: false,
                 cache: false,
@@ -170,7 +174,7 @@
         function do_addGoods() {
             var goods = {"name": "土鸡蛋", "price": 2.4, "inventory": 5000, "simpledescribe": "新鲜鸡蛋"}
             $.ajax({
-                url: 'http://127.0.0.1:8080/addGoods',
+                url: '/addGoods',
                 type: 'POST',
                 async: false,
                 cache: false,
@@ -190,7 +194,7 @@
         function do_updateGoods() {
             var goods = {"goodsName": $("#goodsName").val()}
             $.ajax({
-                url: 'http://127.0.0.1:8080/updateGoods',
+                url: '/updateGoods',
                 type: 'POST',
                 async: false,
                 cache: false,
@@ -205,16 +209,40 @@
                 }
             });
         }
-        function do_searchOrder() {			
-			var goodsName = $("#goodsName").val()
+        function do_searchOrder() {
             $.ajax({
-                url: 'http://127.0.0.1:8080/searchOrder?state='+goodsName,
+                url: '/searchOrder',
                 type: 'GET',
                 async: false,
                 cache: false,
                 processData: false,
                 success: function (returndata) {
                     alert(returndata);
+                    for(var i=0;i<returndata.length;i++){
+                        var id = returndata[i].id;
+                        $('#buyer_info').append('<li>');
+                        $('#buyer_info').append(id);
+                        $('#buyer_info').append('</li>');
+                    }
+
+                },
+                error: function (returndata) {
+                    alert(returndata);
+                }
+            });
+        }
+        function do_putInBuyerList(id){
+            $.ajax({
+                url: '/searchOrder',
+                type: 'GET',
+                async: false,
+                cache: false,
+                processData: false,
+                success: function (returndata) {
+                    for(var i=0;i<length(returndata);i++){
+                        returndata[i].id
+                    }
+                    alert(returndata)
                 },
                 error: function (returndata) {
                     alert(returndata);
@@ -224,7 +252,7 @@
         function do_createOrder(){
             var initOrderInfo = {"expect":"2019-12-04 19:08:29","goodsMap":{1:10,2:25,9:10,10:20}}/*{id:weight}*/
             $.ajax({
-                url: 'http://127.0.0.1:8080/createOrder',
+                url: '/createOrder',
                 type: 'POST',
                 async: false,
                 cache: false,
